8d6eb75b196151270058b934414afea3fd8eca2f
xido <erivery2@gmail.com>
2018-12-11 16:26:05 +0900

    make jquery view banner area 01

--- b_example/c_normal/js/src/make_jquery_01_result.js
+++ b_example/c_normal/js/src/make_jquery_01_result.js
@@ -6,22 +6,23 @@
   $('body').html('<div id="wrap"></div>');
   var wrap = $('#wrap');
   wrap.append('<header id="headBox"></header>\
-                     <section id="viewBox"></section>\
-                     <article id="newsBox"></article>\
-                     <article id="conBox"></article>\
-                     <footer id="footBox"></footer>');
+               <section id="viewBox"></section>\
+               <article id="newsBox"></article>\
+               <article id="conBox"></article>\
+               <footer id="footBox"></footer>');
 
 
 var wrapChild = wrap.children();
 console.log(wrapChild);
 
 var headBox = $(wrapChild[0]);
-var viewBox = $('#viewBox');
+var viewBox = $(wrapChild[1]);
 var newsBox = $('#newsBox');
 var conBox  = $('#conBox');
 var footBox = $('#footBox');
 // headBox.css({height:'10rem', backgroundColor:'#faa'});
 
+// #headBox ==============================================
 headBox.html('<h1></h1>\
               <nav id="gnb">\
                 <ul></ul>\
@@ -35,6 +36,8 @@ var h1Link = h1.children('a');
 // h1Link.text('회사의 이름을 입력')
 // h1Link.attr('href','../../index.html');
 h1Link.text('회사의 이름을 입력').attr('href','../../index.html');
+
+// #gnb ------------------------------------------------------
 
 $('#gnb').prepend('<h2>global navigation</h2>');
 
@@ -63,15 +66,141 @@ for( ; i < 12; i+=1 ){
 
 
 $('#headBox').nextAll().html('<h2>이곳에는 해당영영의 이름이 들어감!</h2>');
+// #viewBox ==========================================
 
+// 01. 각영역(인디케이트, 버튼부,광고배너영역부) 나누어서 틀 삽입
+viewBox.append('<div class="view_btn"></div>');
+viewBox.append('<div class="view_slide_btn"></div>');
+viewBox.append('<div class="view_banner"></div>');
+
+// 02. 슬라이드 버튼부(이전,이후내용보기버튼) 설정
+$('.view_slide_btn').append('<button type="button">다음내용 보기</button');
+$('.view_slide_btn').append('<button type="button">이전내용 보기</button');
+
+// 03. 광고 내용을 만들기
+// 03-1. 광고에들어갈 이미지와, 제목, 내용, 링크경로를 각각 설정하여 data화 처리
+    // - 객체형식의 내부에 들어가는 속성명과 속성값의 형태는 모두 쌍따옴표로 감싸서 처리(차후, JSON 형태)
+
+var viewContent = [
+      {"idName": "link_01", "img":"img_01.jpg", "title":"해당광고 제목1", "content":"무언가 내용_1", "link":"naver.com"},
+      {"idName": "link_02", "img":"img_02.jpg", "title":"해당광고 제목2", "content":"무언가 내용_2", "link":"daum.net"},
+      {"idName": "link_03", "img":"img_03.jpg", "title":"해당광고 제목3", "content":"무언가 내용_3", "link":"google.com"},
+      {"idName": "link_04", "img":"img_04.jpg", "title":"해당광고 제목4", "content":"무언가 내용_4", "link":"behance.net"}
+    ];
+
+// 03-2. 광고 형식에 맞는 기초 틀 제작(샘플링)
+var bannerSample = '<div id="link_01">\
+                      <h3>광고내용제목_01</h3>\
+                      <p>광고설명_01</p>\
+                      <div class="view_link"><a href="http://link_01.com">자세히보기</a></div>\
+                    </div>';
+
+// 03-3. 1차 간단하고 무식하게 집어넣기/2차 수정까지
+var viewBanner = $('.view_banner');
+// viewBanner.append(bannerSample);
+// viewBanner.append(bannerSample);
+// viewBanner.append(bannerSample);
+// viewBanner.append(bannerSample);
+
+var viewChild = viewBanner.children();
+
+/* //** 최대한 이해하기 쉽게 작성 
+  // 각각 id수정
+  viewChild.eq(0).attr('id','link_01');
+  viewChild.eq(1).attr('id','link_02');
+  viewChild.eq(2).attr('id','link_03');
+  viewChild.eq(3).attr('id','link_04');
+
+  // 제목의 값 수정
+  viewChild.eq(0).find('h3').text('광고 제목_1');
+  viewChild.eq(1).find('h3').text('광고 제목_2');
+  viewChild.eq(2).find('h3').text('광고 제목_3');
+  viewChild.eq(3).find('h3').text('광고 제목_4');
+
+  // 내용의 값 수정
+  viewChild.eq(0).find('p').text('광고 내용을 작성_1');
+  viewChild.eq(1).find('p').text('광고 내용을 작성_2');
+  viewChild.eq(2).find('p').text('광고 내용을 작성_3');
+  viewChild.eq(3).find('p').text('광고 내용을 작성_4');
+
+  // 링크의 값 수정
+  // .view_banner>div:nth-child(n)>div>a{}
+  // .view_banner>div:nth-child(n) a{}
+  viewChild.eq(0).find('a').attr({'href':'naver.com'});
+  viewChild.eq(1).find('a').attr({'href':'daum.net'});
+  viewChild.eq(2).find('a').attr({'href':'google.com'});
+  viewChild.eq(3).find('a').attr({'href':'behance.net'});
+*/
+
+// 03-4
+/* // ** 위의 객체형태를 활용하여 내용을 삽입하도록 하자!!!
+  // 각각 id수정   viewContent[1].idName
+  viewChild.eq(0).attr( 'id',viewContent[0].idName );
+  viewChild.eq(1).attr( 'id',viewContent[1].idName );
+  viewChild.eq(2).attr( 'id',viewContent[2].idName );
+  viewChild.eq(3).attr( 'id',viewContent[3].idName );
+
+  // 제목의 값 수정   viewContent[1].title     
+  viewChild.eq(0).find('h3').text( viewContent[0].title );
+  viewChild.eq(1).find('h3').text( viewContent[1].title );
+  viewChild.eq(2).find('h3').text( viewContent[2].title );
+  viewChild.eq(3).find('h3').text( viewContent[3].title );
+
+  // 내용의 값 수정   viewContent[1].content
+  viewChild.eq(0).find('p').text( viewContent[0].content );
+  viewChild.eq(1).find('p').text( viewContent[1].content );
+  viewChild.eq(2).find('p').text( viewContent[2].content );
+  viewChild.eq(3).find('p').text( viewContent[3].content );
+
+  // 링크의 값 수정   viewContent[1].link
+  // .view_banner>div:nth-child(n)>div>a{}
+  // .view_banner>div:nth-child(n) a{}
+  viewChild.eq(0).find('a').attr({ 'href' : viewContent[0].link });
+  viewChild.eq(1).find('a').attr({ 'href' : viewContent[1].link });
+  viewChild.eq(2).find('a').attr({ 'href' : viewContent[2].link });
+  viewChild.eq(3).find('a').attr({ 'href' : viewContent[3].link });
+*/
+
+// 03-5 반복기능을 통해 보다 쉽고 빠르게 처리
+var i = 0;
+var conLen = viewContent.length;
+var viewChild;
+for(; i<conLen; i++){
+  viewBanner.append(bannerSample);  
+  viewChild = viewBanner.children();
+
+  viewChild.eq(i).attr( 'id',viewContent[i].idName );
+  viewChild.eq(i).find('h3').text( viewContent[i].title );
+  viewChild.eq(i).find('p').text( viewContent[i].content );
+  viewChild.eq(i).find('a').attr({ 'href' : viewContent[i].link });
+}
+
+// 04. 인디케이터를 제작
+var viewBtn = $('.view_btn');
+viewBtn.append('<ul></ul>');
+var viewIndi = viewBtn.children('ul');
+var indiSample = '<li><a href="#link_01">narr_01</a></li>';
+
+i=0;
+var  viewIndiChild;
+
+for(; i<conLen; i+=1){
+  viewIndi.append(indiSample);
+  viewIndiChild = viewIndi.children();
+
+  viewIndiChild.eq(i).find('a').attr('href', '#'+viewContent[i].idName);
+  viewIndiChild.eq(i).find('a').text(viewContent[i].content);
+}
+
+// #newsBox =========================================================
 $('#newsBox').append('<div class="news_outer"></div>');
-var newsBox = '<div class="news_01">\
-               <div class="img"><span>뉴스이미지1</span></div>\
-               <dl class="news_content">\
-                 <dt>news_title</dt>\
-                 <dd>news_content</dd>\
-               </dl>\
-               </div>';
+var newsBox = '<div class="news news_01">\
+                 <div class="img"><span>뉴스이미지1</span></div>\
+                 <dl class="news_content">\
+                   <dt>news_title</dt>\
+                   <dd>news_content</dd>\
+                 </dl>\
+                 </div>';
 
 var newsOuter = $('.news_outer');
 // var imgUrl = "../../../img/news/";
@@ -91,17 +220,28 @@ var newsContent = [
     {className:'news_13', img:'#f12', imgNarr:'m번째 이미지', tit:'title_13', con:'con_13'}
     ];
  
-var newsSelector, i = 0;
-for(; i < 12; i+=1){
-  newsOuter.append(newsBox);
+// 변수 newsContent의 n번째 해당하는 배열요소의 내부에있는 객체속성명의 속성값을 가져와라!
+// newsContent[0].img
 
+var newsSelector, i = 0;
+for(; i < newsContent.length ; i+=1){
+  newsOuter.append(newsBox);
   newsSelector = newsOuter.children('div').eq(i);
-  newsSelector.attr({'class':newsContent[i].className});
+  newsSelector.attr({'class':'news'+' '+newsContent[i].className, 'title':'추가 속성을 사용하여 입력!!!'});
   newsSelector.find('.img').css({backgroundColor:newsContent[i].img});
   newsSelector.find('.img').children('span').text(newsContent[i].imgNarr);
   newsSelector.find('.news_content').children('dt').text(newsContent[i].tit);
   newsSelector.find('.news_content').children('dd').text(newsContent[i].con);
 }
+
+
+
+
+
+
+
+
+
 
 
 
