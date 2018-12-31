cf225d1fdacbbe5e800daa42058a02915afcb3ea
xido <erivery2@gmail.com>
2018-12-31 16:50:55 +0900

    jquery on 메소드

--- b_example/b_basic/js/src/jquery_on_01.js
+++ b_example/b_basic/js/src/jquery_on_01.js
@@ -33,8 +33,63 @@
 
 // ----------------------------------------
 
+ var mouse = $('.mouse');
+ mouse.on('mousedown',function(e) {
+  console.log(e.which);
+  var num = e.which;
+/*
+  if(num == 1){
+    $(this).children('span').text('마우스 왼버튼을 눌렀습니다.');
+  }else if(num == 2){
+    $(this).children('span').text('마우스 가운데버튼 눌렀습니다.');
+  }else if(num == 3){
+    $(this).children('span').text('마우스 오른버튼 눌렀습니다.');
+  }else{
+    $(this).children('span').text('그런버튼 안키웁니다.!!!');
+  }
+*/
 
+  switch(num){
+    case 1:
+      $(this).children('span').text('마우스 왼버튼을 눌렀습니다.');
+      break;
 
+    case 2:
+      $(this).children('span').text('마우스 가운데버튼 눌렀습니다.');
+      break;
+
+    case 3:
+      $(this).children('span').text('마우스 오른버튼 눌렀습니다.');
+      break;
+
+    default:
+      $(this).children('span').text('그런버튼 안키웁니다.!!!');
+  }
+
+ });
+
+ mouse.on('mouseup',function(e) {
+  $(this).children('span').text('마우스를 띄었습니다.');
+ });
+
+//  =========================
+ var fc = $('.focus');
+ var fcList = fc.children('li');
+
+ fcList.children().on('focus',function(){
+    $(this).css({backgroundColor:'#07a', color:'#fff', fontWeight:'bold'});
+ });
+
+ fcList.children().on('blur',function() {
+    $(this).removeAttr('style');
+ });
+
+fcList.children().on('mouseenter',function() {
+   $(this).css({backgroundColor:'#999'});
+});
+fcList.children().on('mouseleave',function() {
+   $(this).css({backgroundColor:'#fa0'});
+});
 
 })(jQuery);
 
