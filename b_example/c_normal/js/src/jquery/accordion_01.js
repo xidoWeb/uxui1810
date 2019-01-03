e4ca6b76fb965d4ddc331818237d9b4e9602152f
xido <erivery2@gmail.com>
2019-01-03 11:43:58 +0900

    아코디언 세로형  focus기능으로 처리

--- b_example/c_normal/js/src/jquery/accordion_01.js
+++ b_example/c_normal/js/src/jquery/accordion_01.js
@@ -6,31 +6,40 @@
   var accorDd  = accordion.find('dd');
   // console.log(accorBtn, accorDd);
 
-  accorDd.eq(0).show();
+   accorDd.eq(0).show();
+   accorBtn.eq(0).addClass('active');
 
-  accorBtn.on('click focus',function(e) {
+  accorBtn.on('focus',function(e) {
     e.preventDefault();
-    var dd = $(this).parent().next('dd');
-    // dd.css({display:'block'});
-    // dd.slideDown();
 
-    var ddState = dd.css('display') == 'none';
-    if(ddState){
-      dd.slideDown();
-    }else{
-      dd.slideUp();
-    }
-    // dd.slideToggle();
+    var _this = $(this);
+    var dd = _this.parent().next('dd');
+    // dd.css({display:'block'});
+    dd.slideDown();
+
+    // var ddState = dd.css('display') == 'none';
+    // if(ddState){      dd.slideDown();    }else{      dd.slideUp();    }
+    // // dd.slideToggle();
 
 
     var ddS = dd.siblings('dd');
     // ddS.css({display:'none'});
     // ddS.removeAttr('style');
     ddS.slideUp();
+
+
+    //  --------------
+    // accorBtn.removeClass('active');
+    _this.addClass('active');
+    var dtButton = _this.parent().siblings('dt').children('button');
+    dtButton.removeClass('active');
+
   });
 
   accorBtn.on('blur',function(){
-    var dd = $(this).parent().next('dd');
+    var _this = $(this);
+    var dd    = _this.parent().next('dd');
+    _this.removeClass('active');
     dd.slideUp();
   });
 
