d4dfc7c4de37bea02fd3368e42014526d126d3e7
xido <erivery2@gmail.com>
2019-01-03 11:02:06 +0900

    horizontal accordion  menu slide기능추가

--- b_example/c_normal/js/src/jquery/accordion_01.js
+++ b_example/c_normal/js/src/jquery/accordion_01.js
@@ -1,5 +1,38 @@
 //  accordion_01.js
 
 (function($) {
+  var accordion = $('.accordion');
+  var accorBtn = accordion.find('dt').children('button');
+  var accorDd  = accordion.find('dd');
+  // console.log(accorBtn, accorDd);
+
+  accorDd.eq(0).show();
+
+  accorBtn.on('click focus',function(e) {
+    e.preventDefault();
+    var dd = $(this).parent().next('dd');
+    // dd.css({display:'block'});
+    // dd.slideDown();
+
+    var ddState = dd.css('display') == 'none';
+    if(ddState){
+      dd.slideDown();
+    }else{
+      dd.slideUp();
+    }
+    // dd.slideToggle();
+
+
+    var ddS = dd.siblings('dd');
+    // ddS.css({display:'none'});
+    // ddS.removeAttr('style');
+    ddS.slideUp();
+  });
+
+  accorBtn.on('blur',function(){
+    var dd = $(this).parent().next('dd');
+    dd.slideUp();
+  });
+
 
 })(jQuery);
