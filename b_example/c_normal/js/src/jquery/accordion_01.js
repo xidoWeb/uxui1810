5fece808faa449ba2128c0f0f8d897a2fdf0e22b
xido <erivery2@gmail.com>
2019-01-03 13:55:34 +0900

    accordion bubble 현상 처리

--- b_example/c_normal/js/src/jquery/accordion_01.js
+++ b_example/c_normal/js/src/jquery/accordion_01.js
@@ -3,37 +3,27 @@
 (function($) {
   var accordion = $('.accordion');
   var accorBtn = accordion.find('dt').children('button');
-  var accorDd  = accordion.find('dd');
-  // console.log(accorBtn, accorDd);
+  
+  accorDd.eq(0).show();
+  accorBtn.eq(0).addClass('active');
 
-   accorDd.eq(0).show();
-   accorBtn.eq(0).addClass('active');
-
-  accorBtn.on('focus',function(e) {
+  accorBtn.on('click focus',function(e) {
     e.preventDefault();
+    var type = e.bubbles;
+    
+    if(type){
+      var _this = $(this);
+      var dd = _this.parent().next('dd');
 
-    var _this = $(this);
-    var dd = _this.parent().next('dd');
-    // dd.css({display:'block'});
-    dd.slideDown();
+       dd.slideToggle();
 
-    // var ddState = dd.css('display') == 'none';
-    // if(ddState){      dd.slideDown();    }else{      dd.slideUp();    }
-    // dd.slideToggle();
+      var ddS = dd.siblings('dd');
+      ddS.slideUp();
 
-
-    var ddS = dd.siblings('dd');
-    // ddS.css({display:'none'});
-    // ddS.removeAttr('style');
-    ddS.slideUp();
-
-
-    //  --------------
-    // accorBtn.removeClass('active');
-    _this.addClass('active');
-    var dtButton = _this.parent().siblings('dt').children('button');
-    dtButton.removeClass('active');
-
+      _this.addClass('active');
+      var dtButton = _this.parent().siblings('dt').children('button');
+      dtButton.removeClass('active');
+    }
   });
 
   accorBtn.on('blur',function(){
@@ -42,6 +32,5 @@
     _this.removeClass('active');
     dd.slideUp();
   });
-
 
 })(jQuery);
