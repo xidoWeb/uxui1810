4312d8a79bba9528cc24ed1416252bc880f5d69c
xido <erivery2@gmail.com>
2019-01-11 17:59:16 +0900

    monfee banner

--- b_example/c_normal/js/src/monfee/main_layout.js
+++ b_example/c_normal/js/src/monfee/main_layout.js
@@ -3,7 +3,30 @@
   // * 기준 html문서를 기준으로 불러와야 함!!!
   // * 반드시 서버가 동작해야 함!!!
 
+  var wrap = $('#wrap');
+  var allUrl = './all/';
+
   // 1. header#headBox 생성 및 header.html불러오기
-  console.log('!!!!!!!!! are you ready??????');
-  
+  wrap.prepend('<header id="headBox"></header>');
+  var headBox = $('#headBox');
+
+  var link = function(file, url, fileName){
+    // if(file){  var f = file;  }else{   var f = 'header';  }
+    var f  = file     || 'header';
+    var u  = url      || allUrl;
+    var fn = fileName || '.html';
+
+    var result = u+f+fn;
+    console.log(result);
+    return result;
+  };
+  headBox.load(allUrl + 'header.html');
+
+// 2. section#adBanner를 생성하여 불러오기
+headBox.after('<section id="adBanner"></section>');
+var adBanner = $('#adBanner');
+var mainUrl = './main/';
+adBanner.load(mainUrl + 'adBanner.html');
+
+
 })(jQuery);
