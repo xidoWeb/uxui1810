cf953982c6d2a17aef98ff8a9944b4db0428e509
xido <erivery2@gmail.com>
2019-01-11 15:19:41 +0900

    스크롤위치에 따라 화면표기방법

--- b_example/c_normal/js/src/monfee/head_01.js
+++ b_example/c_normal/js/src/monfee/head_01.js
@@ -6,16 +6,30 @@
   var headCon    = headBox.find('.container');
   var gnb        = headBox.find('#gnb');
   // var headDd     = headBox.find('dd');
-
+  var winH = win.height();
+  var scrollOffset = 0;
 
     // 스크롤처리시 
   win.on('scroll', function(e){
     // 스크롤이 얼만큼 움직였는지 파악
-    var scrollOffset = $(this).scrollTop();
+    scrollOffset = $(this).scrollTop();
 
     // 스크롤양과, headOffset의 크기값을 비교하여 처리
     var vs = headOffset <= scrollOffset;
-    (vs) ?  headBox.css({position:'fixed', top:0, bottom:'auto'})  :  headBox.removeAttr('style')  ;
+    // (vs) ?  headBox.css({position:'fixed', top:0, bottom:'auto'})  :  headBox.removeAttr('style');
+    if(vs) {  headBox.css({position:'fixed', top:0, bottom:'auto'}); } else { headBox.removeAttr('style');}
+
+    //1-2의 문제 해결: 화면의 높이를 기준으로 절반아래로 header가 있으면 위로 하위메뉴가 나오게 처리
+    var heightVs = scrollOffset >= winH/2; 
+    console.log(heightVs);
+    if(!heightVs){
+      // heightVs의 값이false인경우
+      headCon.addClass('up');
+    }else{
+      // heightVs의 값이 true인경우
+      headCon.removeClass('up');
+    }
+
   });
 
 
@@ -31,7 +45,7 @@
               };
 
 
-  // gnb.on({'mouseenter': addView , 'mouseleave': removeView });
+  gnb.on({'mouseenter': addView , 'mouseleave': removeView });
 
 
   // 기능1의 2번째 처리시 부차적 문제: header위치가 하단에 있는경우와 상단에 있는경우에 따라, 
