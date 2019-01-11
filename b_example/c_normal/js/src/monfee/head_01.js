bd3bcf1b8495ae06f6d7667c8f0398728dd64381
xido <erivery2@gmail.com>
2019-01-11 14:51:11 +0900

    monfee 마우스올렸을경우처리

--- b_example/c_normal/js/src/monfee/head_01.js
+++ b_example/c_normal/js/src/monfee/head_01.js
@@ -1,9 +1,13 @@
 (function($){
   // 기능1: header기능이 스크롤처리해서 상단에 올라오면 position:fixed 처리
-  var win    = $(window);
-  var headBox = $('#headBox');
+  var win        = $(window);
+  var headBox    = $('#headBox');
   var headOffset = headBox.offset().top;
-  
+  var headCon    = headBox.find('.container');
+  var gnb        = headBox.find('#gnb');
+  // var headDd     = headBox.find('dd');
+
+
     // 스크롤처리시 
   win.on('scroll', function(e){
     // 스크롤이 얼만큼 움직였는지 파악
@@ -16,6 +20,24 @@
 
 
   // 기능1의 2번째 처리: header메뉴에 마우스 올리거나 focus되었을때, 하위 메뉴가 나타나게 만들기.
-  // 기능1의 2번째 처리시 부차적 문제: header메뉴에 마우스 올리거나 focus되었을때, 하위 메뉴는 어떻게 나타나야 하는가?
-  
+  var addView = function(e){
+                e.preventDefault();
+                headCon.addClass('view');
+              };
+
+  var removeView = function(e){
+                e.preventDefault();
+                headCon.removeClass('view');
+              };
+
+
+  // gnb.on({'mouseenter': addView , 'mouseleave': removeView });
+
+
+  // 기능1의 2번째 처리시 부차적 문제: header위치가 하단에 있는경우와 상단에 있는경우에 따라, 
+  // 하위 메뉴는 상황에따라 보이는형태가 달라야 하지 않을까?
+  // 전체를 기준으로 봐야하는데? 이 전체는 어느 크기일까? ( $(window).height();  )
+
+
+
 })(jQuery);
