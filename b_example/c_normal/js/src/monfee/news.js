1bc4e482000d21228d6ee2393d226bca4af6e478
xido <erivery2@gmail.com>
2019-01-16 14:58:33 +0900

    $.ajax 로 처리하여 외부 data 불러오기

--- b_example/c_normal/js/src/monfee/news.js
+++ b_example/c_normal/js/src/monfee/news.js
@@ -7,31 +7,36 @@
 
 
   // 링크,  사진, 제목, 내용
-var imgUrl = "../../../img/monfee/new_menu/";
-var newsArray = [
-    {"link":"url_1", "pic":"menu_01.png", "tit":"title_a", "con":"content_1"},
-    {"link":"url_2", "pic":"menu_02.png", "tit":"title_b", "con":"content_2"},
-    {"link":"url_3", "pic":"menu_03.png", "tit":"title_c", "con":"content_3"},
-    {"link":"url_4", "pic":"menu_04.png", "tit":"title_d", "con":"content_4"},
-    {"link":"url_5", "pic":"menu_05.png", "tit":"title_e", "con":"content_5"},
-    {"link":"url_6", "pic":"menu_06.png", "tit":"title_f", "con":"content_6"},
-    {"link":"url_7", "pic":"menu_07.png", "tit":"title_g", "con":"content_7"}
-  ];
+var imgUrl = "../../img/monfee/new_menu/";
+
+// 비동기식(병렬) 방식으로 처리되어 외부의 data를 불러오는 형식은 모두 
+// A(async:비동기식)J(javascript:자바스크립트로)A(and:그리고)X(xml:외부문서)를 불러오는 방식
+var jsonDataUrl = "../../data/newsArray.json";
+var newsArray = $.ajax({type:'get', async:false, url:jsonDataUrl}).responseJSON;
+
 
   // newsCon.append(newsList);
   var i = 0, list;
-  for (; i < newsArray.length ; i += 1){
+  var AppendListMenu = function(i){
     newsCon.append(newsList);
-
     list = $('.news_list').eq(i);
-    list.find('a').attr({'href': newsArray[i].link});
-    list.find('.img_list').css({ 'backgroundImage': 'url(' + imgUrl + newsArray[i].pic +')'});
-    list.find('dt').text( newsArray[i].tit );
-    list.find('dd').text( newsArray[i].con );
+    list.find('a').attr({ 'href': newsArray[i].link });
+    list.find('.img_list').css({ 'backgroundImage': 'url(' + imgUrl + newsArray[i].pic + ')' });
+    list.find('dt').text(newsArray[i].tit);
+    list.find('dd').text(newsArray[i].con);
+  };
+
+  for (; i < newsArray.length ; i += 1){
+    AppendListMenu(i);
   }
 
 
 
 
 
+/*
+    var url = '/data/newsArray.json';
+    // var newsData = $.ajax({type:'get', url:url, async:false}).responseJSON;
+    // var newsData = $.ajax({type:'get', url:url, async:false}).responseText;
+*/
 })(jQuery);
