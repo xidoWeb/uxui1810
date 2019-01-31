bdcf7f67e26cd9dba5e551540ced2fb32d725dc1
xido <erivery2@gmail.com>
2019-01-31 12:52:40 +0900

    mousewheel 기능 수행에대한 내용(전체)

--- b_example/c_normal/js/src/jquery/horizontal_scroll.js
+++ b_example/c_normal/js/src/jquery/horizontal_scroll.js
@@ -17,25 +17,106 @@
   var i = 0;
 // viewBox.scrollLeft();
 
-  samplebtn.on('click', function(){
-    i +=1 ;
-    per -= viewLiWidth;
-    if(i < voLi.length){
-      viewOuterUl.animate({marginLeft:per});
-    }
-  });
+  // samplebtn.on('click', function(){
+  //   i +=1 ;
+  //   per = 1 * -100 + '%; //1
+  //   per -= viewLiWidth;  //2
+  //   if(i < voLi.length){
+  //     viewOuterUl.animate({marginLeft:per});
+  //   }
+  // });
 
 
 
 // 스크롤바를 보이지 않게 수정
   var viewCon = viewOuterUl.wrap('<div class="scroll_box">');
-  $('.scroll_box').css({width:'100%', height:'100%',paddingBottom:'3rem', overflowX:'auto'} );
   viewOuter.css({overflow:'hidden'});
+  $('.scroll_box').css({width:'100%', height:viewOuter.innerHeight(), paddingBottom:'3rem', overflow:'hidden', overflowX:'auto'} );
 
 
-  viewOuter.on('scroll', function(){
-     var sl = $(this).scrollLeft();
-    console.log(sl);
-  });
+  // viewOuter.on('scroll', function(){
+  //    var sl = $(this).scrollLeft();
+  //   console.log(sl);
+  // });
+
+// 마우스 스크롤처리
+
+ var go = true;
+viewOuter.on('mousewheel DOMMouseScroll', function(e){
+  e.preventDefault();
+  // console.log(e); 
+
+  if(go){
+    go = false;
+    var E = e.originalEvent;
+    var delta; 
+
+    if(E.wheelDelta){
+      delta = E.wheelDelta;
+    }else{
+      delta = E.detail * -40;
+    }
+    console.log(delta);
+
+    if(delta < 0 && i < voLi.length-1){
+      // 마우스를 아래로 휠을 움직였을경우
+      i +=1;
+    }else if(delta > 0 && i > 0){
+      // 마우스를 위로 휠을 움직였을경우
+      i -=1;
+    }
+   // console.log(i);
+   /* // marginleft기능으로 값을 이동처리
+   per = i * -100 + '%';  // 1 * -100 => -100
+   viewOuterUl.stop().animate({marginLeft:per},function(){
+       go = true;
+   }); 
+   */
+  
+    // 가로스크롤 기능으로 이동처리
+    per = i * viewLiWidth;
+    $('.scroll_box').animate({scrollLeft:per}, function(){
+      go = true;
+    });
+    console.log(-per);  
+  }
+});
+
+close.on('click',function(){
+  viewBox.remove();
+});
+// viewBox.hide();
+// -----------------------------------------
+
+var conBox = $('#contBox');
+var body = $('html') || $('body');
+var conBoxChild = conBox.children('div').eq(0).outerWidth();
+var wheelresult = 0;
+var myscroll = true;
+conBox.on('mousewheel', function(e){
+  var E = e.originalEvent;
+  var delta = E.wheelDelta;
+  if(myscroll){
+    myscroll = false;
+      if(delta < 0){ 
+        if(wheelresult < conBox.outerWidth()){
+          wheelresult += conBoxChild; 
+        }
+      }else{ 
+        if(wheelresult > 0){
+          wheelresult -= conBoxChild;
+        }
+       }
+
+      body.stop().animate({scrollLeft:wheelresult},function(){
+        console.log($(this));
+        myscroll = true;
+      });
+    // console.log(wheelresult);
+
+  }
+});
+
+
 
 })(jQuery);
