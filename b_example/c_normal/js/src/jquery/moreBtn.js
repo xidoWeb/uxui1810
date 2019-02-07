dde283f4c09d6e00d54ee71105b923a878650ade
xido <erivery2@gmail.com>
2019-02-07 10:24:59 +0900

    목록 만들기

--- b_example/c_normal/js/src/jquery/moreBtn.js
+++ b_example/c_normal/js/src/jquery/moreBtn.js
@@ -7,7 +7,7 @@ var people = $.ajax({
     url:'../../data/people.json',
     async:false
   }).responseJSON;
-// console.log(people);
+console.log(people);
 var peopleLen = people.length;
 
 // ==========================================
@@ -22,6 +22,25 @@ var listSample = '<li>\
 // ==========================================
 // 반복기능을통해 목록 전체 담기
 // 1테스트: li하나 담기
+var peopleEl = $('.people').children('ul');
+
+var peopleFor = function(n){
+  peopleEl.append(listSample);
+  var peopleLi = peopleEl.find('li').eq(n);
+  var peopleArr = people[n];
+  var peopleName = peopleArr.first_name + ' ' + peopleArr.last_name;
+
+  peopleLi.find('.key').text( peopleArr.id );
+  peopleLi.find('.name').text( peopleName );
+  peopleLi.find('.mail').text( peopleArr.email );
+  peopleLi.find('.contry').text( peopleArr.country );
+  peopleLi.find('.day').text( peopleArr.modified );
+};
+
+var i=0;
+for(; i<peopleLen; i++){
+  peopleFor(i);
+}
 
 // ==========================================
 // 일정갯수만 보이기
