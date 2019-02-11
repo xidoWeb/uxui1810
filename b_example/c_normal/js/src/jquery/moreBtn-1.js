280b76b39f21ec81ec25547d968a1157e3c771b3
xido <erivery2@gmail.com>
2019-02-11 18:14:06 +0900

    morebtn

--- b_example/c_normal/js/src/jquery/moreBtn-1.js
+++ b_example/c_normal/js/src/jquery/moreBtn-1.js
@@ -1,4 +1,5 @@
-// moreBtn.js
+// moreBtn-1.js
+
 (function($) {
 // 외부 데이터 불러오기
 
@@ -7,7 +8,7 @@ var people = $.ajax({
     url:'../../data/people.json',
     async:false
   }).responseJSON;
-console.log(people);
+// console.log(people);
 var peopleLen = people.length;
 
 // var test = people.slice(10,15);
@@ -26,50 +27,67 @@ var listSample = '<li>\
 // 반복기능을통해 목록 전체 담기
 // 1테스트: li하나 담기
 var peopleEl = $('.people').children('ul');
+for(var k=0; k<10; k++){
+  peopleEl.append(listSample);  
+}
 
-var peopleFor = function(n){
-  if( people[n] ){
-    peopleEl.append(listSample);
+var peopleFor = function(n, index){
+
+    // x는 index값이 들어왔을경우에 index*10+n값으로 처리하고, index값이 없다면, n의 값으로 대체
+    // if(index){ var x= n+(index*10); }else{ var x = n; }
+    var x = n+(index*10) || n;  
+
     var peopleLi = peopleEl.find('li').eq(n);
-    var peopleArr = people[n];
+    // console.log(n + '번째 li를 선택');
+    var peopleArr = people[x];
     var peopleName = peopleArr.first_name + ' ' + peopleArr.last_name;
     peopleLi.find('.key').text( peopleArr.id );
     peopleLi.find('.name').text( peopleName );
     peopleLi.find('.mail').text( peopleArr.email );
     peopleLi.find('.contry').text( peopleArr.country );
     peopleLi.find('.day').text( peopleArr.modified );
-  }
+    // console.log(n);
 };
 
-// var i=0;
-// for(; i<peopleLen; i++){
-//   peopleFor(i);
-// }
 
 // ==========================================
 // 일정갯수만 보이기
 
-var i=0;
-var view = 10; // 보이고자 하는 갯수  
-var forGo = function(view){
+var forGo = function(index){
+  var i=0;
+  var view = 10; // 보이고자 하는 갯수  
+  var jsonlist= index || 0;
   for(; i<view; i++){
-      peopleFor(i);
-      console.log(i);
+      peopleFor(i, jsonlist);
+  console.log('!!');
   }
 };
-forGo(view);
+forGo();
 
-//  더보기 버튼 클릭시처리
+//  더보기 버튼 삭제, 목록 인디케이터생성
 var btn = $('.more_btn');
-btn.on('click', function(e){
+btn.remove();
+
+peopleEl.after('<ol class="list_indi"></ol>');
+var indi = peopleEl.next('.list_indi');
+// 생성할 인디케이터 갯수 파악
+var viewList = Math.ceil(peopleLen/10);
+// console.log(viewList);
+var j=0;
+for(; j<viewList; j++){
+  indi.append('<li><button type="button">'+ (j+1) +'</button></li>');
+}
+
+var indiLi = indi.find('li');
+indiLi.on('click', function(e){
   e.preventDefault();
-  if(view < peopleLen){
-    view += 10;
-    forGo(view);
-  }else{
-     $(this).hide();
-   }
+  var index = $(this).index();
+  forGo(index);
 });
+
+// ======================================================
+
+
 
 
 })(jQuery);
